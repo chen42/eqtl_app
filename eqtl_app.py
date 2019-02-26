3947b02df3747c2a236f20a3c40a57dd5cac6c0a
Hao Chen <inbox@haochen.name>
2019-02-25 23:15:33 -0600

    D2 line plot working

--- eqtl_app.py
+++ eqtl_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from subprocess import Popen, PIPE
-import sys
+import json 
+import pandas as pd
+import numpy as np
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '#DtfrL98G5t1dC*4'
@@ -20,9 +22,15 @@ def plots():
         chr, bp=term["loc"].split(":")
         process=Popen(['bash', 'export_eqtl_data.sh', chr, bp], stdout=PIPE, stderr=PIPE)
         stdout, stderr=process.communicate()
-        #print (bp, file=sys.stderr)
-        return render_template("plots.html", loc=term, data=stdout.split())
-
+        file=str(chr)+"_"+(bp)+"_"+"win"+str(2000000)+".tab"
+        df=pd.read_csv(file, delimiter="\t")
+        dfplot=df.iloc[:,3:5]
+        dfplot.columns=['bp', 'p']
+        dfplot['logp'] = -np.log10(dfplot['p'])
+        chart_data=dfplot.to_dict(orient='records')
+        chart_data=json.dumps(chart_data,indent=2)
+        data={'chart_data':chart_data}
+        return render_template("plots.html", data=data, loc=term)
 if __name__ == '__main__':
     app.run(debug=True)
 
--- export_eqtl_data.sh
+++ export_eqtl_data.sh
@@ -5,7 +5,7 @@ chr=$1
 loc=$2
 
 
-grep -P "$chr\t" ./eqtl_data.tab |awk  -v loc="$2" -v win="$window"  'function abs(value) {return (value<0?-value:value)} { if (abs(loc-$4) < win ) print $0}'
+grep -P "$chr\t" ./eqtl_data.tab |awk  -v loc="$2" -v win="$window"  'function abs(value) {return (value<0?-value:value)} { if (abs(loc-$4) < win ) print $0}' | sort -k4  >$chr\_$loc\_win$window.tab
 
 
 
