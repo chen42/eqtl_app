22b898b52bcb5d7888a15b55ebf7ce8ee56a4232
Hao Chen <inbox@haochen.name>
2019-05-02 05:48:29 -0500

    remove flask code

--- get_all_peaks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#grep chr `ls *loco.txt` |awk '{if ($12<0.00000251188) {print $1, "\t", $2,  "\t", $12}}' |sed "s/_\|:/\t/g" |cut -f 1,2,3,7,8,9
- grep chr `ls *loco.txt` |awk '{if ($15<0.00000251188*5) {print $1, "\t", $2,  "\t", $15}}' |sed "s/_\|:/\t/g" |cut -f 1,2,6,7,8
-
